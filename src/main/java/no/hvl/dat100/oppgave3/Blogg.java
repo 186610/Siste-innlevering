ef19d4f717c2c7e41ffea92dee8100f8e520ce08
h186607 <186607@stud.hvl.no>
2024-11-13 11:38:34 +0100

    done

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -5,68 +5,112 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	private Innlegg[] innleggstabell;
+	private int nesteLedige;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this(20);
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggstabell = new Innlegg[lengde];
+		nesteLedige = 0;
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteLedige;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggstabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		boolean lik = false;
+		int pos = 0;
+		while (pos < nesteLedige && !lik) {
+			if (innleggstabell[pos].erLik(innlegg)) {
+				lik = true;
+			} pos++;
+		}
+		if (lik) {
+			return pos;
+		} return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		for (Innlegg i : innleggstabell) {
+			if (i != null && i.erLik(innlegg)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		if (nesteLedige < innleggstabell.length) {
+			return true;
+		} 
+		return false;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if (finnes(innlegg) || nesteLedige == innleggstabell.length) {
+			return false;
+		} 
+		
+		innleggstabell[nesteLedige] = innlegg;
+		nesteLedige++;
+		return true;
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		String s = getAntall() + "\n";
+		
+		for (int i = 0; i < nesteLedige; i++) {
+			s += innleggstabell[i].toString();
+		}
+		
+		return s;
 	}
 
 	// valgfrie oppgaver nedenfor
 	
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg[] utvidetInnleggstabell = new Innlegg[innleggstabell.length * 2];
+		
+		for (int i = 0; i < innleggstabell.length; i++) {
+			utvidetInnleggstabell[i] = innleggstabell[i];
+		}
+		innleggstabell = utvidetInnleggstabell;
 	}
 	
 	public boolean leggTilUtvid(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if (!finnes(innlegg)) {
+			return false;
+		}  
+			
+		if (nesteLedige >= innleggstabell.length) {
+			utvid();
+		}
 		
+		return leggTil(innlegg);
 	}
 	
 	public boolean slett(Innlegg innlegg) {
 		
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public int[] search(String keyword) {
+		for (int i = 0; i < nesteLedige; i++) {
+			if (innleggstabell[i].erLik(innlegg)) {
+				nesteLedige--;
+				innleggstabell[i] = innleggstabell[nesteLedige];
+				innleggstabell[nesteLedige] = null;
+				return true;
+			}
+		}
 		
-		throw new UnsupportedOperationException(TODO.method());
-
+		return false;
+		
 	}
 }
--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,7 +1,13 @@
 package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
@@ -10,6 +16,31 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		PrintWriter skriver = null;
+		Path mappePath = Paths.get(mappe);
+		
+		try {
+			if (!Files.exists(mappePath, LinkOption.NOFOLLOW_LINKS)) {
+				Files.createDirectories(mappePath);
+			}
+				
+		} catch (IOException e) {
+			return false;
+		}
+		
+		
+		try {
+			skriver = new PrintWriter(mappePath.resolve(filnavn).toString());
+			skriver.print(samling);
+		} catch (FileNotFoundException e) {
+			return false;
+		} finally {
+			if (skriver != null) {
+				skriver.close();
+			}
+		}
+		
+		return true;
 	}
+
 }
